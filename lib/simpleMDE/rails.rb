ab4914516751b196626e912f4a67c823ca8a42f6
Filip Egge <filip.egge@gmail.com>
2015-12-24 22:45:46 +0100

    Add vendor files, version 1.9.0

---
+++ lib/simpleMDE/rails.rb
@@ -0,0 +1,8 @@
+require "simpleMDE/rails/version"
+
+module SimpleMDE
+  module Rails
+    class Engine < ::Rails::Engine
+    end
+  end
+end
---
+++ lib/simpleMDE/rails/version.rb
@@ -0,0 +1,5 @@
+module SimpleMDE
+  module Rails
+    VERSION = "1.9.0"
+  end
+end
